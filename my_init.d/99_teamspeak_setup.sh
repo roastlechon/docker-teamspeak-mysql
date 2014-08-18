e09564a594b25ab1fc6127d9d7bbd2f309ce072b
Noel Madali <amadali@gmail.com>
2014-08-18 01:41:32 -0400

    Copy pasta error. RUN was in teamspeak setup script

--- my_init.d/99_teamspeak_setup.sh
+++ my_init.d/99_teamspeak_setup.sh
@@ -7,8 +7,8 @@ echo '       database=$TS3_MYSQL_DB'
 echo '       username=$TS3_MYSQL_USER'
 echo '       password=$TS3_MYSQL_PASS'
 
-RUN sed -i -e "s/host=127.0.0.1/host=$TS3_MYSQL_HOST/g" /opt/teamspeak/ts3db_mysql.ini
-RUN sed -i -e "s/port=3306/port=$TS3_MYSQL_PORT/g" /opt/teamspeak/ts3db_mysql.ini
-RUN sed -i -e "s/username=ts3/username=$TS3_MYSQL_USER/g" /opt/teamspeak/ts3db_mysql.ini
-RUN sed -i -e "s/password=password/password=$TS3_MYSQL_PASS/g" /opt/teamspeak/ts3db_mysql.ini
-RUN sed -i -e "s/database=ts3/database=$TS3_MYSQL_DB/g" /opt/teamspeak/ts3db_mysql.ini
+sed -i -e "s/host=127.0.0.1/host=$TS3_MYSQL_HOST/g" /opt/teamspeak/ts3db_mysql.ini
+sed -i -e "s/port=3306/port=$TS3_MYSQL_PORT/g" /opt/teamspeak/ts3db_mysql.ini
+sed -i -e "s/username=ts3/username=$TS3_MYSQL_USER/g" /opt/teamspeak/ts3db_mysql.ini
+sed -i -e "s/password=password/password=$TS3_MYSQL_PASS/g" /opt/teamspeak/ts3db_mysql.ini
+sed -i -e "s/database=ts3/database=$TS3_MYSQL_DB/g" /opt/teamspeak/ts3db_mysql.ini
